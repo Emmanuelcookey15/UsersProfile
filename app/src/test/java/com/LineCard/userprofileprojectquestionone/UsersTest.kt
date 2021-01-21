ed5736b06d8babc44fdf8c1e18eb50890d466054
Emmanuelcookey15 <Emmanuelcookey744@gmail.com@users.noreply.github.com>
2021-01-21 11:48:58 +0100

    Wrote Junit test for Users model checking for, user fields not not null, users id field return as integer and not as an string or float

--- app/src/test/java/com/LineCard/userprofileprojectquestionone/UsersTest.kt
+++ app/src/test/java/com/LineCard/userprofileprojectquestionone/UsersTest.kt
@@ -1,4 +1,42 @@
 package com.LineCard.userprofileprojectquestionone
 
+import com.LineCard.userprofileprojectquestionone.model.Users
+import org.junit.Test
+
+import org.junit.Assert.*
+
 class UsersTest {
+
+
+    @Test
+    fun users_fields_isworking(){
+        val aUser = Users()
+        aUser.id = 1
+        aUser.name = "My Name"
+        aUser.email ="myname@mail.com"
+        aUser.username = "my_name"
+        aUser.phone = "990-998-97"
+        assertNotNull(aUser)
+    }
+
+    @Test
+    fun users_field_id_iscorrect_datatype() {
+        val aUser = Users()
+        aUser.id = 1
+        aUser.name = "My Name"
+        assertNotSame(aUser.id, "1")
+    }
+
+    @Test
+    fun users_field_ignored_isempty() {
+        val aUser = Users()
+        aUser.id = 1
+        aUser.name = "My Name"
+        assertEquals(aUser.username, "")
+        assertEquals(aUser.email, "")
+        assertEquals(aUser.phone, "")
+    }
+
+
+
 }
